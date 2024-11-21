20d23f4c27c81e422c02923add0e5f324d2f46d4
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-21 20:51:15 +0700

    menambahkan komponen FormView lengkap dengan input, pemilihan jenis kelamin, dan tombol simpan

--- app/src/main/java/com/example/arsitekturmvvm/ui/view/FormulirView.kt
+++ app/src/main/java/com/example/arsitekturmvvm/ui/view/FormulirView.kt
@@ -1,8 +1,15 @@
 package com.example.arsitekturmvvm.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 //@Preview(showBackground = true)
@@ -33,12 +41,81 @@ fun FormulirView(
         modifier = modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        TextField(
+            value = nama,
+            onValueChange = {nama = it},
+            label = {
+                Text(text = "nama")
+            },
+            placeholder = {
+                Text(text = "isi nama anda")
+            },
+            modifier = modifier.fillMaxWidth().padding(2.dp)
+        )
+        Row (modifier = Modifier.fillMaxWidth()){
+            listJK.forEach{selectedGender->
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = gender == selectedGender,
+                        onClick = {gender = selectedGender}
+                    )
+                    Text(text = selectedGender)}
+            }
+        }
+        TextField(
+            value = nim,
+            onValueChange = {nim = it},
+            label = {
+                Text(text = "nim")
+            },
+            placeholder = {
+                Text(text = "isi NIM anda")
+            },
+            modifier = modifier.fillMaxWidth().padding(2.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
 
 
-
-
-
-
-
+        TextField(
+            value = email,
+            onValueChange = {email = it},
+            label = {
+                Text(text = "email")
+            },
+            placeholder = {
+                Text(text = "isi email anda")
+            },
+            modifier = modifier.fillMaxWidth().padding(2.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        TextField(
+            value = alamat,
+            onValueChange = {alamat = it},
+            label = {
+                Text(text = "alamat")
+            },
+            placeholder = {
+                Text(text = "isi alamat anda")
+            },
+            modifier = modifier.fillMaxWidth().padding(2.dp)
+        )
+        TextField(
+            value = notelepon,
+            onValueChange = {notelepon = it},
+            label = {
+                Text(text = "notelepon")
+            },
+            placeholder = {
+                Text(text = "isi notelepon anda")
+            },
+            modifier = modifier.fillMaxWidth().padding(2.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Button(onClick = {
+            onSubmitClicked(listData)
+        }) {
+            Text(text = "Simpan")
+        }
     }
+
 }
