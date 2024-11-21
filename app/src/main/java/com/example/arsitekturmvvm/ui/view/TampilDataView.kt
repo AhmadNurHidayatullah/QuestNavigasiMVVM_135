9a2c5afd5703c06db501bce4f85c4623841b7761
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-21 21:04:54 +0700

    Menambahkan fungsi TampilData untuk menampilkan label dan isi data

--- app/src/main/java/com/example/arsitekturmvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/arsitekturmvvm/ui/view/TampilDataView.kt
@@ -1,8 +1,13 @@
 package com.example.arsitekturmvvm.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,6 +27,25 @@ fun TampilDataView(uiState: DataSiswa, onBackButton : () -> Unit){
         TampilData(x = "NoTelepon", y = uiState.NoTelepon)
         Button(onClick = onBackButton) {
             Text(text = "kembali")
+        }
+    }
+}
+
+@Composable
+fun TampilData(x : String, y :String){
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween)
+        {
+            Text(text = x,
+                modifier = Modifier.weight(0.8f))
+            Text(text = ":",
+                modifier = Modifier.weight(0.2f))
+            Text(text = y,
+                modifier = Modifier.weight(2f))
         }
     }
 }
