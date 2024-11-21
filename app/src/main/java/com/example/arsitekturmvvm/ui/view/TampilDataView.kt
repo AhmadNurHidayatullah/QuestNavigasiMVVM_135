77c3bccb3fd91c6b18aae34dfc5a0919e33deb60
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-21 21:00:05 +0700

    Menambahkan TampilDataView yang disusun untuk menampilkan detail siswa dengan tombol kembali, masih error pada tampil data karena Belum Menambahkan function TampilData

--- app/src/main/java/com/example/arsitekturmvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/example/arsitekturmvvm/ui/view/TampilDataView.kt
@@ -1,2 +1,27 @@
 package com.example.arsitekturmvvm.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.arsitekturmvvm.model.DataSiswa
+
+@Composable
+fun TampilDataView(uiState: DataSiswa, onBackButton : () -> Unit){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp))
+    {
+        TampilData(x = "Nama", y = uiState.Nama)
+        TampilData(x = "NIM", y = uiState.Nim)
+        TampilData(x = "Jenis Kelamin", y = uiState.Gender)
+        TampilData(x = "Alamat", y = uiState.Alamat)
+        TampilData(x = "Gmail", y = uiState.Email)
+        TampilData(x = "NoTelepon", y = uiState.NoTelepon)
+        Button(onClick = onBackButton) {
+            Text(text = "kembali")
+        }
+    }
+}
