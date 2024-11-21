b6b6479f38570054519f8aa0819c9c5e0c214f7a
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-21 20:34:29 +0700

    Menambahkan DataSiswa model with fields: Nama, Nim, Gender, Alamat, Email, and NoTelepon

--- app/src/main/java/com/example/arsitekturmvvm/model/DataSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataSiswa.kt
@@ -1,3 +1,12 @@
 package com.example.arsitekturmvvm.model
 
+data class DataSiswa(
+    val Nama: String ="",
+    val Nim: String = "",
+    val Gender: String ="",
+    val Alamat: String ="",
+    val Email: String = "",
+    val NoTelepon: String = ""
+){
 
+}
