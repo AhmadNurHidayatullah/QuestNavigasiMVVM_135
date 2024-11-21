5fbb225252c170038f01c91647f2b1209c5e4106
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2024-11-21 20:36:36 +0700

    menambahkan objek JenisKelamin dengan daftar string gender

--- app/src/main/java/com/example/arsitekturmvvm/model/JenisKelamin.kt
+++ app/src/main/java/com/example/arsitekturmvvm/model/JenisKelamin.kt
@@ -1,2 +1,10 @@
 package com.example.arsitekturmvvm.model
 
+import com.example.arsitekturmvvm.R
+
+object JenisKelamin {
+    val JenisJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
